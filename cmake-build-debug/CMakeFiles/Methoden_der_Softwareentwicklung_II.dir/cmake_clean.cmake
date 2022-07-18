5f9b990f3f8b015645681201ff13535de7712623
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2022-07-18 11:28:04 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/character.cpp.obj"
+  "CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/hero.cpp.obj"
+  "CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/item.cpp.obj"
+  "CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/main.cpp.obj"
+  "Methoden_der_Softwareentwicklung_II.exe"
+  "Methoden_der_Softwareentwicklung_II.exe.manifest"
+  "Methoden_der_Softwareentwicklung_II.pdb"
+  "libMethoden_der_Softwareentwicklung_II.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Methoden_der_Softwareentwicklung_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
